916f4733d840757534d98b1771104e795742914f
Samarsh7520 <samarsh.jaiz@gmail.com>
2025-11-30 17:37:27 +0530

    Add files via upload

---
+++ A - 1.py
@@ -0,0 +1,6 @@
+def wishes(n):
+    print("Hello,",n,"How are you?")
+    print("Hope you are doing well")
+
+wishes("Samarsh")
+wishes("Pihu")
---
+++ A - 2.py
@@ -0,0 +1,7 @@
+def weather(w,c):
+    print("Todays weather is",w,)
+    print("It is very ",c)
+
+weather("Sunny", "hot")
+weather("Rainy", "cold")
+weather("Cloudy", "cool")    
